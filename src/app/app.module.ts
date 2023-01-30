b58ab2a93fb94b0fe5f1813f7f9a87974af9a90f
roshanmali <roshanmali1205@gmail.com>
2023-01-30 23:42:59 +0530

    added home profile and about-us component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
